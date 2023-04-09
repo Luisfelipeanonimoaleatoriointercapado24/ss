99ba434089eaff7e1b3e6620d5a72ce838135038
Luisfelipeanonimoaleatoriointercapado24 <114437524+Luisfelipeanonimoaleatoriointercapado24@users.noreply.github.com>
2023-04-09 12:09:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+<b class="name_of_objects">
+Bolinha De Pular
+<hr>
+Caderno
+<hr>
+Relogio
+<hr>
+Caneta
+<hr>
+</b>
+
+<label>Visualisaçao da webcam:</label>
+<div id="camera"></div>
+<br></br>
+<div id="result"></div>
+<br></br>
+<button onclick="take_spashot();" class="btn btn-info">Capturar Imagem</button>
+<br></br>
+<button onclick="check();" class="btn btn-info">Identificar Imagem</button>
+<br></br>
+
+<p
